0f0ace8b26bb616db294490599c00d1aa9188a5c
Prathap <prathapraja2k@gmail.com>
2025-07-15 11:36:17 +0530

    Step 1: Return 0 for empty string

---
+++ test/string_calculator_test.dart
@@ -0,0 +1,10 @@
+import 'package:test/test.dart';
+import 'package:string_calculator/string_calculator.dart';
+
+void main() {
+  group('StringCalculator.add', () {
+    test('should return 0 for empty string', () {
+      expect(StringCalculator().add(''), equals(0));
+    });
+  });
+}
